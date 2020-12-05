64933bb35708c5d84aa4be648fb3995a30f5d2cd
halim <abdulalimgiwa@yahoo.com>
2020-12-05 16:01:15 +0100

    its another day

--- docs/index.js
+++ docs/index.js
@@ -10,4 +10,39 @@
             }
         }
 
-        
+function secondFunction() {
+    var x = document.getElementById("admissions")
+    if (x.style.display = "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
+
+function secondFunction2() {
+    var x = document.getElementById("admissions")
+    if (x.style.display = "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
+
+function thirdFunction() {
+    var x = document.getElementById("About-us")
+    if (x.style.display = "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
+
+function thirdFunction2() {
+    var x = document.getElementById("About-us")
+    if (x.style.display = "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
+
